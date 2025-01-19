8efb9053915d2c28425c21ad3035748bf79381ca
Jeffrey Zhu <1624410543@qq.com>
2025-01-19 14:48:09 +0800

    first commit

---
+++ OnlineStoreBE/src/test/java/com/jeffrey/onlinestorebe/OnlineStoreBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeffrey.onlinestorebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
