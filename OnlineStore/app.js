4d7b8d1cf6ceb08532e35db8b18f570e075d9e9c
Jeffrey Zhu <1624410543@qq.com>
2025-01-21 17:38:12 +0800

    添加弹出框

--- OnlineStore/app.js
+++ OnlineStore/app.js
@@ -1,13 +1,6 @@
 // app.js
 App({
   data:{
-    value: 'label_1',
-    list: [
-      { value: 'label_1', label: '首页', icon: 'home' },
-      { value: 'label_2', label: '应用', icon: 'app' },
-      { value: 'label_3', label: '聊天', icon: 'chat' },
-      { value: 'label_4', label: '我的', icon: 'user' },
-    ],
   },
   methods:{
   },
--- OnlineStore/pages/goodDetail/goodDetail.js
+++ OnlineStore/pages/goodDetail/goodDetail.js
@@ -1,19 +1,53 @@
+/*
+ * @Author: Jeffrey Zhu 1624410543@qq.com
+ * @Date: 2025-01-21 16:31:57
+ * @LastEditors: Jeffrey Zhu 1624410543@qq.com
+ * @LastEditTime: 2025-01-21 17:32:54
+ * @FilePath: \OnlineStore\pages\goodDetail\goodDetail.js
+ * @Description: File Description Here...
+ * 
+ * Copyright (c) 2025 by JeffreyZhu, All Rights Reserved. 
+ */
 // pages/goodDetail/goodDetail.js
+
+
 Page({
   /**
    * 页面的初始数据
    */
   data: {
-    id:"123",
-    title:"Title",
-    detail:"detaildetaildetaildetaildetaildetaildetaildetaildetail",
-    price:123,
-    imgList:[
-      { url:"123" },
-      { url:"123" },
-      { url:"123" },
-      { url:"123" },
+    id: "123",
+    goodName: "goodName",
+    detail: "detaildetaildetaildetaildetaildetaildetaildetaildetail",
+    price: 123,
+    visible: false,
+    imgList: [
+      { url: "123" },
+      { url: "123" },
+      { url: "123" },
+      { url: "123" },
     ]
+  },
+
+  methods: {
+    handleBack() {
+      wx.navigateBack();
+    }
+  },
+  onClickIcon() {
+    console.log('点击图标');
+  },
+
+  onClickButton() {
+    console.log('点击按钮');
+  },
+  handlePopup(e) {
+    this.setData({ visible: true });
+  },
+  onVisibleChange(e) {
+    this.setData({
+      visible: e.detail.visible,
+    });
   },
   /**
    * 生命周期函数--监听页面加载
