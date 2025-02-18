bad386f503f3729ee59dad1c625124995e76a063
Jeffrey Zhu <1624410543@qq.com>
2025-02-18 15:46:00 +0800

    主页商品搜索栏

--- onlineStore/pages/index/index.js
+++ onlineStore/pages/index/index.js
@@ -2,9 +2,25 @@ const app = getApp()
 
 Page({
   data: {
-    
+    value:"",
   },
   onShow() {
 		this.getTabBar().init();
-	},
+  },
+  onSearch() {
+    // wx.switchTab({
+    //   url: 'url',
+    //   success: (res) => {},
+    //   fail: (res) => {},
+    //   complete: (res) => {},
+    // })
+  },
+  onClick() {
+    
+  },
+  onChange(e) {
+    this.setData({
+      value: e.detail,
+    });
+  },
 })
