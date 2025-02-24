fbfd0e36191b47a32f5527dcb18cc2e893cac12f
Jeffrey Zhu <1624410543@qq.com>
2025-02-24 22:01:42 +0800

    卸载material

--- admin-dashboard/src/index.tsx
+++ admin-dashboard/src/index.tsx
@@ -2,7 +2,7 @@
  * @Author: Jeffrey Zhu 1624410543@qq.com
  * @Date: 2025-02-24 14:00:33
  * @LastEditors: Jeffrey Zhu 1624410543@qq.com
- * @LastEditTime: 2025-02-24 14:59:25
+ * @LastEditTime: 2025-02-24 22:01:38
  * @FilePath: \onlineStore\admin-dashboard\src\index.tsx
  * @Description: File Description Here...
  * 
@@ -13,11 +13,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement );
