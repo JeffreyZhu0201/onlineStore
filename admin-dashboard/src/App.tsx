a986fc4fe77685b76e937908f0bb28a1e4060bca
Jeffrey Zhu <1624410543@qq.com>
2025-02-24 15:00:35 +0800

    添加typescript，基础配置完成

---
+++ admin-dashboard/src/App.tsx
@@ -0,0 +1,21 @@
+/*
+ * @Author: Jeffrey Zhu 1624410543@qq.com
+ * @Date: 2025-02-24 14:00:33
+ * @LastEditors: Jeffrey Zhu 1624410543@qq.com
+ * @LastEditTime: 2025-02-24 14:59:44
+ * @FilePath: \onlineStore\admin-dashboard\src\App.tsx
+ * @Description: File Description Here...
+ * 
+ * Copyright (c) 2025 by JeffreyZhu, All Rights Reserved. 
+ */
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-red-500">
+      123
+    </div>
+  );
+}
+
+export default App;
---
+++ admin-dashboard/src/index.tsx
@@ -0,0 +1,33 @@
+/*
+ * @Author: Jeffrey Zhu 1624410543@qq.com
+ * @Date: 2025-02-24 14:00:33
+ * @LastEditors: Jeffrey Zhu 1624410543@qq.com
+ * @LastEditTime: 2025-02-24 14:59:25
+ * @FilePath: \onlineStore\admin-dashboard\src\index.tsx
+ * @Description: File Description Here...
+ * 
+ * Copyright (c) 2025 by JeffreyZhu, All Rights Reserved. 
+ */
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement );
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
